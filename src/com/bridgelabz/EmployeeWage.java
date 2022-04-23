95815c485d084fd61064d682cd159567d55607a4
ashishmashal <ashishmashal5@gmail.com>
2022-04-23 18:39:12 +0530

    Added: Employee Daily Wage is  Added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -9,9 +9,13 @@ public class EmployeeWage {
         Random random = new Random();
         int present = random.nextInt(2);
         //Check if employee is present
+        int WagePerHr =20;
+        int wrkHrs = 8;
+        int dailyWage = present * WagePerHr * wrkHrs;
+
         if (present == 1)
-            System.out.println("Employee is present.");
+            System.out.println("Employee is present and Daily Wage Is " + dailyWage);
         else
-            System.out.println("Employee is absent.");
+            System.out.println("Employee is absent so Daily Wage is "+ dailyWage);
     }
 }
