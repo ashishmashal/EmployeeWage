81965c273d1ee2245262778ab3dbcb59d2f45e4a
ashishmashal <ashishmashal5@gmail.com>
2022-04-23 18:31:16 +0530

    Added: Emplyee Attedance Added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome");
+
+        Random random = new Random();
+        int present = random.nextInt(2);
+        //Check if employee is present
+        if (present == 1)
+            System.out.println("Employee is present.");
+        else
+            System.out.println("Employee is absent.");
     }
 }
